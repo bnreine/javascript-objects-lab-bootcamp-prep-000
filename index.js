50f7fb2e92bcae597d679ba258af057f1c954ea3
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-19 18:04:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,27 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObject;
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  var appendObject={};
+  appendObject[key]=value;
+  return Object.assign(object,object,appendObject);
+}
 
 /*
+describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
+    it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
+      var obj = { prop: 1 }
+
+      expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+
+      expect(obj).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+  })
 
   */
